da91cedd775f0bb211efca57d6e2edd543d6fa9c
noeliajudithzelaya <noeliajudithzelayautnfrt@gmail.com>
2025-02-11 19:37:07 -0300

    se agrego a .env la SECRET_KEY

--- controllers/user.js
+++ controllers/user.js
@@ -1,119 +1,143 @@
-const { validationResult } = require("express-validator")
-const UserModel = require("../models/user")
-const bcrypt = require('bcryptjs')
-const { JsonWebTokenError } = require("jsonwebtoken")
+require("dotenv").config();
+const { validationResult } = require("express-validator");
+const UserModel = require("../models/user");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-const getAllUser = async(req, res) => {
-   try {
-    const allUsers = await UserModel.find()
-    res.status(200).json({msg:'Se envian todos los usuarios', allUsers})
-   } catch (error) {
-    console.log(error)    
-    res.status(400).json({msg: 'No se encontraron usuarios'})
-   }
-}
+const getAllUser = async (req, res) => {
+  try {
+    const allUsers = await UserModel.find();
+    res.status(200).json({ msg: "Se envían todos los usuarios", allUsers });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ msg: "No se encontraron usuarios" });
+  }
+};
 
-const getOneUser = async(req, res) => {
-    try {
-     const id = req.params.id
-     const getUser = await UserModel.findOne({_id:id})
-     res.status(200).json({msg:'Usuario encontrado', getUser})
-    } catch (error) {
-     console.log(error)    
-     res.status(404).json({msg: 'No se encontro el usuario'})
+const getOneUser = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const getUser = await UserModel.findOne({ _id: id });
+    if (!getUser) {
+      return res.status(404).json({ msg: "Usuario no encontrado" });
     }
- }
+    res.status(200).json({ msg: "Usuario encontrado", getUser });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: "Error en el servidor" });
+  }
+};
 
-const createUser = async(req, res ) => {
-    try {
-        const errors = validationResult(req)
-    if(!errors.isEmpty()){
-        return res.status(400).json({msg: errors.array()})
+const createUser = async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ msg: errors.array() });
     }
-        const body= req.body
-        const userExist =await UserModel.findOne({
-            usuario: body.usuario
-        })
-        if(userExist){
-            return res.status(400).json({msg: 'El usuario ya existe'})}
 
-            const salt = await bcrypt.genSaltSync()
-            body.contrasenia = await bcrypt.hash(body.contrasenia, salt)
-            const user= new UserModel(body)
-            await user.save() 
-            console.log(user)
-            res.status(201).json({msg:'Usuario creado'})
-        } catch (error) {
-            console.log(error)       
-    }
-}
+    const { usuario, contrasenia } = req.body;
+    const userExist = await UserModel.findOne({ usuario });
 
-const updateUser = async ( req, res) => {
-    try {
-        const errors = validationResult(req)
-    if(!errors.isEmpty()){
-        return res.status(400).json({msg: errors.array()})
+    if (userExist) {
+      return res.status(400).json({ msg: "El usuario ya existe" });
     }
-        const updateUser = await UserModel.findByIdAndUpdate({_id: req.params.id}, req.body, {new: true})
-        res.status(200).json({msg: 'Usuario actualizado'})
 
-    } catch (error) {
-        console.log(error)
-        res.status(400).json({msg:'No se actualizo el usuario'})
+    const salt = await bcrypt.genSaltSync();
+    const hashedPassword = await bcrypt.hash(contrasenia, salt);
+
+    const newUser = new UserModel({ usuario, contrasenia: hashedPassword });
+    await newUser.save();
+
+    res.status(201).json({ msg: "Usuario creado correctamente" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: "Error en el servidor" });
+  }
+};
+
+const updateUser = async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ msg: errors.array() });
     }
-}
+
+    const updateUser = await UserModel.findByIdAndUpdate(
+      { _id: req.params.id },
+      req.body,
+      { new: true }
+    );
+
+    if (!updateUser) {
+      return res.status(404).json({ msg: "Usuario no encontrado" });
+    }
+
+    res.status(200).json({ msg: "Usuario actualizado correctamente", updateUser });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: "Error en el servidor" });
+  }
+};
 
 const deleteUser = async (req, res) => {
-    try {
-        await UserModel.findByIdAndDelete({_id:req.params.id})
-        res.status(200).json({msg:'usuario eliminado'})
-    } catch (error) {
-        console.log(error)
-        res.status(404).json({msg:'no se elimino el usuario'})
-    }
-}
+  try {
+    const userDeleted = await UserModel.findByIdAndDelete({ _id: req.params.id });
 
-const loginUser= async(req, res) => {
-    try {
-        const errors = validationResult(req)
-    if(!errors.isEmpty()){
-        return res.status(400).json({msg: errors.array()})
+    if (!userDeleted) {
+      return res.status(404).json({ msg: "Usuario no encontrado" });
     }
-    const {usuario, contrasenia} = req.body
-    const userExist = await UserModel.findOne({usuario})
-    if(!userExist){
-        return res.status(400).json({msg:'el usuario no existe'})
-    }
-    const passCheck= await bcrypt.compare( contrasenia, userExist.contrasenia)
-    res.send('Usuario logueado')
-    if(passCheck){
-        const jwPayload = {
-            usuario: {
-                id: userExist.id,
-                username: userExist.usuario
-            }
-        }
-        const token  = jwt.sign(jwtPayload, process.env.SECRET_KEY)
-        userExist.token= token
-        const userUpdate = await UserModel.findByIdAndUpdate({_id:userExist._id, userExist, new: true})
-        res.status(200).json({msg: 'usuario logueado'})
-    }else{
-        res.status(422).json({msg: 'usuario y/o contraseña incorrectos'})
-    }
-} catch (error) {
-        console.log(error)
-    }
-}
 
-/* const logoutUser = async (req, res) => {
-    const userId = await UserModel.findOne({req.userLoginId})
-    userId.token =''
-    const userLogout = await UserModel.findByIdAndUpdate(
-        {_id: req.userLoginId}, userId, {new: true
-    })
-    res.status(200).json({msg:'usuario deslogueado'})
-} */
+    res.status(200).json({ msg: "Usuario eliminado correctamente" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: "Error en el servidor" });
+  }
+};
+
+const loginUser = async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ msg: errors.array() });
+    }
+
+    const { usuario, contrasenia } = req.body;
+    const userExist = await UserModel.findOne({ usuario });
+
+    if (!userExist) {
+      return res.status(400).json({ msg: "El usuario no existe" });
+    }
+
+    const passCheck = await bcrypt.compare(contrasenia, userExist.contrasenia);
+    if (!passCheck) {
+      return res.status(422).json({ msg: "Usuario y/o contraseña incorrectos" });
+    }
+
+    const jwPayload = {
+      usuario: {
+        id: userExist.id,
+        username: userExist.usuario,
+      },
+    };
+
+    const token = jwt.sign(jwPayload, process.env.SECRET_KEY, { expiresIn: "1h" });
+
+    const role = userExist.role || "user";
+
+    await UserModel.findByIdAndUpdate(userExist._id, { token });
+
+    res.status(200).json({ token, role });
+  } catch (error) {
+    console.error("Error en login:", error);
+    res.status(500).json({ msg: "Error en el servidor" });
+  }
+};
 
 module.exports = {
-    getAllUser, createUser, updateUser, deleteUser, loginUser/* , logoutUser */
-}
+  getAllUser,
+  getOneUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  loginUser,
+};
