933c96170ca501e51040089ba13b259d1346a56a
noeliajudithzelaya <noeliajudithzelayautnfrt@gmail.com>
2025-07-18 14:44:50 -0300

    se vuelve a una version anterior

--- api/index.js
+++ api/index.js
@@ -2,11 +2,8 @@ const serverless = require('serverless-http');
 const Server = require('../server/server');
 const conectarbd = require('../dataBase/config');
 
-// Instanciás tu app
 const server = new Server();
 
-// Conectás a la base de datos
 conectarbd();
 
-// Exportás la app adaptada como función serverless
 module.exports = serverless(server.app);
