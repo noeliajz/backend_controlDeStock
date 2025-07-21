0f49f8d72ee8d9c1e2e53e9a66f86b890f55c98f
noeliajudithzelaya <noeliajudithzelayautnfrt@gmail.com>
2025-07-21 15:47:26 -0300

    se eliminan comentarios

--- dataBase/config.js
+++ dataBase/config.js
@@ -1,25 +1,23 @@
 const mongoose = require('mongoose');
-require('dotenv').config(); // Cargar variables de entorno
-
+require('dotenv').config(); 
 const conectarbd = async () => {
     try {
-        const uri = process.env.MONGO_CONNECT; // ← Aquí usamos MONGO_CONNECT
+        const uri = process.env.MONGO_CONNECT; 
         if (!uri) {
             throw new Error(" ERROR: La variable de entorno MONGO_CONNECT no está definida.");
         }
 
-        console.log('🔎 Intentando conectar a:', uri); // Depuración
+        console.log('🔎 Intentando conectar a:', uri); 
 
         await mongoose.connect(uri, {
             tls: true,
             tlsAllowInvalidCertificates: false
         });
 
-        console.log(' Conectado a la base de datos'); // Mueve este log aquí
-
+        console.log(' Conectado a la base de datos'); 
     } catch (error) {
         console.error(' No se pudo conectar con MongoDB:', error.message);
-        process.exit(1); // Detener la ejecución si falla la conexión
+        process.exit(1); 
     }
 };
 
